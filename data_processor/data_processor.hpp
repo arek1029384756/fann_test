ad216c71a3832a70c5b4bbdb87808e0a8cbfa4ce
Arek <arek@arek>
2017-09-16 12:24:20 +0200

    Added start/stop control of data processor thread

--- data_processor/data_processor.hpp
+++ data_processor/data_processor.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <thread>
+#include <atomic>
 #include <chrono>
 #include <string>
 
@@ -10,11 +11,19 @@ namespace data_processor {
 
     class DataProcessor {
         std::thread m_th;
+        std::atomic<uint8_t> m_continue;
 
         void go() {
-            for(int i = 0; i < 50; ++i) {
-                std::cout << "i: " << i << std::endl;
+            int i = 0;
+            while(m_continue.load()) {
+                std::cout << "i: " << i++ << std::endl;
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+        }
+
+        void join() {
+            if(m_th.joinable()) {
+                m_th.join();
             }
         }
 
@@ -30,19 +39,22 @@ namespace data_processor {
 
         virtual ~DataProcessor() {
             std::cout << __func__ << "(), this: " << this << std::endl;
-            if(m_th.joinable()) {
-                m_th.join();    
-            }
+            stop();
         }
 
         void start() {
             if(!m_th.joinable()) {
+                m_continue.store(1);
                 m_th = std::thread([&]() { go(); });
             } else {
                 throw std::runtime_error(std::string("Data processor thread start failed"));
             }
         }
 
+        void stop() {
+            m_continue.store(0);
+            join();
+        }
     };
 
 }
